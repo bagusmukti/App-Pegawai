f18da4887301bbc14d306f898a4d67defad214bb
Bagus Mukti Purnomo <muktib301@gmail.com>
2025-09-25 22:40:06 +0700

    Penambahan Master Layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -6,6 +6,9 @@
     <title>Daftar Pegawai</title>
 </head>
 <body>
+    @extends('master')
+    @section('title', 'Daftar Pegawai')
+    @section('content')
     <div class="container mt-5">
         <h1 class="mb-4">Daftar Pegawai</h1>
     <table border="1" cellpadding="5" cellspacing="0">
@@ -45,5 +48,6 @@
         </tbody>
     </table>
     </div>
+    @endsection
 </body>
 </html>
