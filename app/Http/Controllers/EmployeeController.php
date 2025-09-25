1ba1b189b65b2b28128749e325017793137128c1
Bagus Mukti Purnomo <muktib301@gmail.com>
2025-09-25 17:28:12 +0700

    CRUD table employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
@@ -11,7 +11,8 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        //
+        $employees = Employee::latest()->paginate(5);
+        return view('employees.index', compact('employees'));
     }
 
     /**
@@ -19,7 +20,7 @@ class EmployeeController extends Controller
      */
     public function create()
     {
-        //
+        return view('employees.create');
     }
 
     /**
@@ -27,7 +28,17 @@ class EmployeeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nama_lengkap'  => 'required|string|max:255',
+            'email'         => 'required|email|max:255',
+            'nomor_telepon' => 'required|string|max:20',
+            'tanggal_lahir' => 'required|date',
+            'alamat'        => 'required|string|max:255',
+            'tanggal_masuk' => 'required|date',
+            'status'        => 'required|string|max:50',
+        ]);
+        Employee::create($request->all());
+        return redirect()->route('employees.index');
     }
 
     /**
@@ -35,7 +46,8 @@ class EmployeeController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $employee = Employee::find($id);
+        return view('employees.show', compact('employee'));
     }
 
     /**
@@ -43,7 +55,8 @@ class EmployeeController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $employee = Employee::find($id);
+        return view('employees.edit', compact('employee'));
     }
 
     /**
@@ -51,7 +64,26 @@ class EmployeeController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'nama_lengkap' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'nomor_telepon' => 'required|string|max:20',
+            'tanggal_lahir' => 'required|date',
+            'alamat' => 'required|string|max:255',
+            'tanggal_masuk' => 'required|date',
+            'status' => 'required|string|max:50',
+        ]);
+        $employee = Employee::findOrFail($id);
+        $employee->update($request->only([
+            'nama_lengkap',
+            'email',
+            'nomor_telepon',
+            'tanggal_lahir',
+            'alamat',
+            'tanggal_masuk',
+            'status',
+        ]));
+        return redirect()->route('employees.index');
     }
 
     /**
@@ -59,6 +91,8 @@ class EmployeeController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $employee = Employee::find($id);
+        $employee->delete();
+        return redirect()->route('employees.index');
     }
 }
